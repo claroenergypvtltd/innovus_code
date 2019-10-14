271b32e98ebf48aac3d21baa82748e52c600677a
gis-monica <monica.a@greatinnovus.com>
2019-10-14 16:44:01 +0530

    dashboard_changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,75 @@
 import React, { Component } from 'react';
+import {
+    PieChart, Pie, Sector, Cell, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, BarChart, Bar
 
+} from 'recharts';
+const pieChartdata = [
+    { name: 'Group A', value: 400 },
+    { name: 'Group B', value: 300 },
+    { name: 'Group C', value: 300 },
+    { name: 'Group D', value: 200 },
+];
+const lineChartdata = [
+    {
+        name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+    },
+    {
+        name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+    },
+    {
+        name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+    },
+    {
+        name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+    },
+    {
+        name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+    },
+    {
+        name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+    },
+    {
+        name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+    },
+];
+const barChartdata = [
+    {
+        name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+    },
+    {
+        name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+    },
+    {
+        name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+    },
+    {
+        name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+    },
+    {
+        name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+    },
+    {
+        name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+    },
+    {
+        name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+    },
+];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+    cx, cy, midAngle, innerRadius, outerRadius, percent, index,
+}) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+            {`${(percent * 100).toFixed(0)}%`}
+        </text>
+    );
+};
 export default class Home extends Component {
 
     listPath = () => {
@@ -13,7 +83,7 @@ export default class Home extends Component {
                     <div className="col-md-4 pr-0 dashboard-bx new-order">
                         <a href="#" className="card">
                             <div className="box">
-                                <h5>New Orders</h5>
+                                <h5 className="dashboard-title">New Orders</h5>
                                 <span>45236</span>
                             </div>
                         </a>
@@ -21,7 +91,7 @@ export default class Home extends Component {
                     <div className="col-md-4 pr-0 dashboard-bx delivery-order">
                         <a href="#" className="card">
                             <div className="box">
-                                <h5>Delivery Orders</h5>
+                                <h5 className="dashboard-title">Delivery Orders</h5>
                                 <span>45236</span>
                             </div>
                         </a>
@@ -29,11 +99,109 @@ export default class Home extends Component {
                     <div className="col-md-4 pr-0 dashboard-bx revenue">
                         <a href="#" className="card">
                             <div className="box">
-                                <h5>Revenue</h5>
+                                <h5 className="dashboard-title">Revenue</h5>
                                 <span>45236</span>
                             </div>
                         </a>
                     </div>
+                    <div className="LineChart main-wrapper chart-box">
+                        {/* <div><div className="graph-board">
+                            <h4>User Analytics</h4>
+                            <p>This Week</p>     </div>
+                            <div><p>Farmer</p>
+                                <p>Retailer</p></div>
+                        </div> */}
+                        <div className="main-graph">
+                            <span className="graph-title">User Analytics</span>
+                            <span className="graph-label">This Week</span>
+                        </div>
+                        <div className="sub-graph">
+                            <span className="farm-graph">Farmer</span>
+                            <span className="chart-graph">Retailer</span>
+                        </div>
+                        <PieChart width={400} height={400}>
+                            <Pie
+                                data={pieChartdata}
+                                cx={200}
+                                cy={200}
+                                labelLine={false}
+                                label={renderCustomizedLabel}
+                                outerRadius={80}
+                                fill="#8884d8"
+                                dataKey="value"
+                            >
+                                {
+                                    pieChartdata.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+                                }
+                            </Pie>
+                        </PieChart>
+                        {/* <LineChart
+                            width={500}
+                            height={300}
+                            data={lineChartdata}
+                            margin={{
+                                top: 5, right: 30, left: 20, bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                        </LineChart> */}
+                    </div>
+                    <div className="LineChart main-wrapper chart-box">
+                        <div className="main-graph">
+                            <span className="graph-title">Delivery Orders</span>
+                            <span className="graph-label">This Week</span>
+                        </div>
+                        <div className="sub-graph pb-4">
+                            <span className="order-graph">Orders</span>
+                            <span className="chart-graph">Estimate</span>
+                        </div>
+                        {/* <ul className="graph-board pb-4">
+                            <li>Delivery Orders</li>
+                            <li>This Week</li>
+                            <li>Orders</li>
+                            <li>Estimate</li>
+                        </ul> */}
+                        <BarChart
+                            width={500}
+                            height={300}
+                            data={barChartdata}
+                            margin={{
+                                top: 5, right: 30, left: 20, bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey="pv" fill="#8884d8" />
+                            <Bar dataKey="uv" fill="#82ca9d" />
+                        </BarChart>
+                    </div>
+                    {/* <div className="LineChart main-wrapper">
+                        <PieChart width={400} height={400}>
+                            <Pie
+                                data={pieChartdata}
+                                cx={200}
+                                cy={200}
+                                labelLine={false}
+                                label={renderCustomizedLabel}
+                                outerRadius={80}
+                                fill="#8884d8"
+                                dataKey="value"
+                            >
+                                {
+                                    pieChartdata.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+                                }
+                            </Pie>
+                        </PieChart>
+                    </div> */}
 
                 </div>
                 <div className="row dashboard-graph">
