ebdc98395af6a543d266a8fb28bdf324c43c5c67
gisnasar <nasar.k@greatinnovus.com>
2020-02-10 21:22:33 +0530

    Report1&6

--- src/shared/Reactgraphcharts.js
+++ src/shared/Reactgraphcharts.js
@@ -164,29 +164,49 @@ export function ReactPieChart(props) {
 }
 
 
-export function LineChartView(props) {
+export class LineChartView extends Component {
 
-    let value = props && props.label
+    constructor(props) {
+        super(props)
+        this.state = {
+            getBarChart: true
+        }
+    }
 
-    return (
-        <div>
-            <LineChart
-                width={500}
-                height={400}
-                data={barChartdata}
-                margin={{
-                    top: 5, right: 30, left: 20, bottom: 5,
-                }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" label={{ value: value, position: 'bottom', offset: 5 }} />
-                <YAxis />
-                <Tooltip />
-                <Legend verticalAlign="top" height={36} />
-                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
-        </div>
-    );
-    // }
+    render() {
+        let value = this.props && this.props.label
+        const reactbarChartdata = this.props.barChartData ? this.props.barChartData :
+            [
+                {
+                    name: 'Page A', uv: 4000, pv: 2400, amt: 5000,
+                },
+                {
+                    name: 'Page B', uv: 3000, pv: 1398, amt: 6500,
+                },
+                {
+                    name: 'Page C', uv: 2000, pv: 3800, amt: 2290,
+                },
+            ];
+
+        return (
+            <div>
+                <LineChart
+                    width={500}
+                    height={400}
+                    data={reactbarChartdata}
+                    margin={{
+                        top: 5, right: 30, left: 20, bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" label={{ value: value, position: 'bottom', offset: 5 }} />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend verticalAlign="top" height={36} />
+                    <Line type="monotone" dataKey="Users" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
+                </LineChart>
+            </div>
+        );
+    }
 }
