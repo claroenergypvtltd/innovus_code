a5b6ee21481424c0a3ade41d9dee0240ee89edff
gis-monica <monica.a@greatinnovus.com>
2020-02-28 16:35:59 +0530

    Report and Dashboard Changes

--- src/shared/Reactgraphcharts.js
+++ src/shared/Reactgraphcharts.js
@@ -47,7 +47,43 @@ const RADIAN = Math.PI / 180;
 //     );
 // };
 
+/******************************* DASHBOARD **********************************/
+export function ReactBarChartDash(props) {
+    const reactbarChartdata = props.barChartData ? props.barChartData :
+        [
+            {
+                name: 'Page A', uv: 4000, pv: 2400, amt: 5000,
+            },
+            {
+                name: 'Page B', uv: 3000, pv: 1398, amt: 6500,
+            },
+            {
+                name: 'Page C', uv: 2000, pv: 3800, amt: 2290,
+            },
+        ];
 
+    return (
+        <div className="d-flex justify-content-center">
+            <BarChart
+                width={450}
+                height={400}
+                data={reactbarChartdata}
+                margin={{
+                    top: 5, right: 30, left: 20, bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="Users" fill="#8884d8" />
+                {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
+            </BarChart></div>
+    )
+}
+
+/***************************** REPORT1-Customer Onboard ******************************/
 export function ReactBarChart(props) {
     const reactbarChartdata = props.barChartData ? props.barChartData :
         [
@@ -65,8 +101,8 @@ export function ReactBarChart(props) {
     return (
         <div className="d-flex justify-content-center">
             <BarChart
-                width={800}
-                height={400}
+                width={900}
+                height={500}
                 data={reactbarChartdata}
                 margin={{
                     top: 5, right: 30, left: 20, bottom: 25,
